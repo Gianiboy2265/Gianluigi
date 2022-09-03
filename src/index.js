5b1650d0537913623f334dea80a0ab49e8376b1e
Gianiboy2265 <86840255+Gianiboy2265@users.noreply.github.com>
2022-09-03 13:46:25 +0000

    ik

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv"
-import { Client } from "discord.js";
-
-config();
-
-const client = new Client({ intents: ["Guilds", "GuildMessages"] });
-const token = process.env.token
-
-client.login(token);
